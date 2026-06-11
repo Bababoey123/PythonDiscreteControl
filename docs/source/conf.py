5ebb5a0a41cee67e6e93786d9d32578e85bb9e2b
Adam Caulier <adamcauliera@gmail.com>
2026-06-11 17:09:42 +0200

    Docs but not working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+#sphinx-build -b html docs/source docs/build
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,11 +13,14 @@ author = 'Adam Caulier'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # For Google/NumPy docstrings
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
